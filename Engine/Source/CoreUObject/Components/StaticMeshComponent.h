249ca78091ca7a8576d4dd198dbcbeb12a2e61d4
OhJeongHun <45549589+ojh6507@users.noreply.github.com>
2025-03-24 01:46:31 +0900

    feat : UStaticMesh Getter, Setter 추가

--- Engine/Source/CoreUObject/Components/StaticMeshComponent.h
+++ Engine/Source/CoreUObject/Components/StaticMeshComponent.h
@@ -1,15 +1,19 @@
 ﻿#pragma once
 #include "MeshComponent.h"
 #include "GameFramework/StaticMesh.h"
+
 class UStaticMeshComponent : public UMeshComponent
 {
     UCLASS(UStaticMeshComponent, UMeshComponent)
-    
 public:
     UStaticMeshComponent() = default;
-    UStaticMesh* StaticMesh;
-public:
     virtual void BeginPlay() override;
     virtual void Tick(float DeltaTime) override;
-    
+
+    UStaticMesh* GetStaticMesh() const { return StaticMesh; }
+    void SetStaticMesh(UStaticMesh* InStaticMesh) { StaticMesh = InStaticMesh; }
+
+   
+private:
+    UStaticMesh* StaticMesh;
 };
--- Engine/Source/Engine/GameFrameWork/StaticMesh.h
+++ Engine/Source/Engine/GameFrameWork/StaticMesh.h
@@ -8,9 +8,11 @@ class UStaticMesh : public UObject
     UCLASS(UStaticMesh, UObject)
 
 public:
-
     void SetStaticMeshAsset(FStaticMesh* InStaticMesh);
     FString GetAssetPathFileName();
+    FStaticMesh* GetStaticMeshAsset() const {
+        return StaticMeshAsset;
+    }
 private:
     FStaticMesh* StaticMeshAsset;
 };
